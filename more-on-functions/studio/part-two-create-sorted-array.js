29d935a6dd626379186ab665b22e0304200df1a5
Grant Tracey <gtracey24@gmail.com>
2024-01-29 21:53:42 -0600

    finished exercises

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -8,7 +8,8 @@ function findMinValue(arr){
   return min;
 }
 
-//Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
+//Create a function with an array of numbers as its parameter.
+//This function will return a new array with the numbers sorted from least to greatest value.
 
 /*Within the function:
 1) Define a new, empty array to hold the final sorted numbers.
@@ -19,6 +20,15 @@ function findMinValue(arr){
 6) Be sure to print the results in order to verify your code.*/
 
 //Your function here...
+function leastToGreatest(a) {
+  let sorted = [];
+  let l = a.length;
+  for (let i = 0; [i] < l; i++) {
+    sorted.push(findMinValue(a));
+    a.splice(a.indexOf(findMinValue(a)),1)
+    } 
+    return sorted;
+}
 
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
@@ -27,3 +37,5 @@ function findMinValue(arr){
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+console.log(leastToGreatest(nums1));
