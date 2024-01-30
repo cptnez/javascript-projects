208ca78621697c064853948e817941eecf585dbb
gtracey24 <146043771+cptnez@users.noreply.github.com>
2024-01-29 21:56:25 -0600

    Finished exercises

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -1,13 +1,41 @@
+function findMinValue(arr){
+  let min = arr[0];
+  for (i = 0; i < arr.length; i++){
+    if (arr[i] < min){
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+//Create a function with an array of numbers as its parameter.
+//This function will return a new array with the numbers sorted from least to greatest value.
+
+/*Within the function:
+1) Define a new, empty array to hold the final sorted numbers.
+2) Use the findMinValue function to find the minimum value in the old array.
+3) Add the minimum value to the new array, and remove the minimum value from the old array.
+4) Repeat parts b & c until the old array is empty.
+5) Return the new sorted array.
+6) Be sure to print the results in order to verify your code.*/
+
+//Your function here...
+function leastToGreatest(a) {
+  let sorted = [];
+  let l = a.length;
+  for (let i = 0; [i] < l; i++) {
+    sorted.push(findMinValue(a));
+    a.splice(a.indexOf(findMinValue(a)),1)
+    } 
+    return sorted;
+}
+
+/* BONUS MISSION: Refactor your sorting function to use recursion below:
+ */
+
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
-//Sort each array in ascending order.
-console.log(nums1.sort(function(a, b){return a-b}));
-console.log(nums2.sort(function(a, b){return a-b}));
-console.log(nums3.sort(function(a, b){return a-b}));
-//Sort each array in decending order.
-console.log(nums1.sort(function(a, b){return b-a}));
-console.log(nums2.sort(function(a, b){return b-a}));
-console.log(nums3.sort(function(a, b){return b-a}));
+console.log(leastToGreatest(nums1));
