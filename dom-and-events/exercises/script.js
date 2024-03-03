0544ae8f8b28c7388f5d952283a7eb345454a7f3
gtracey24 <146043771+cptnez@users.noreply.github.com>
2024-03-03 13:55:05 -0600

    Finished Exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,28 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+     
+     missionAbort.addEventListener("mouseover", function (event) {
+        event.target.style.backgroundColor = "red";
+     });
+
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+
+     missionAbort.addEventListener("click", function( event ) {
+        let response = window.confirm('Are you sure you want to abort the mission?');
+        // Code does NOT continue until user responds to confirm window
+            if (response) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+                }
+            }
+     )};
     // Put your code for the exercises here.
     
-}
+
 
 window.addEventListener("load", init);
